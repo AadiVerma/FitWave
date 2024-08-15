fcfeb291e4aa4c965a8c6ea83ba09f271eb11007
Aditya Verma <adityakaplish11@gmail.com>
2024-08-15 19:49:23 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='text-xl bg-black flex justify-center place-items-center text-green-500 h-[100vh] '>
+     FitWave
+    </div>
+  )
+}
+
+export default App
