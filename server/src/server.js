73e0d9490a28398b0cf9fb4175dbdc621fd4539c
Aditya Verma <adityakaplish11@gmail.com>
2024-08-25 11:40:36 +0530

    email and otp stuffs

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,16 @@ import User from './models/userModel.js';
 import DBConnect from './config/dbConfig.js';
 import dbURI from "./config/envConfig.js"
 import loginUser from './services/authService.js';
+import otpsending from './validations/emailvalidations.js'
+import verifyotp from './validations/verifyotp.js'
 DBConnect(dbURI.dbURI);
 const app=express();
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.json({msg:"Hello World"});
 })
+app.use('/email',otpsending);
+app.use('/verifyotp',verifyotp);
 app.post('/users',async (req,res)=>{
     // console.log(req.body);
     await loginUser.signupUser(req,res);
