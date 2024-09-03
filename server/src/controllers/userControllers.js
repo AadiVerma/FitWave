7d73fdecd14f8216dce7837b6c0c6306f2324dbf
Aditya Verma <adityakaplish11@gmail.com>
2024-09-03 11:25:52 +0530

    changes

--- server/src/controllers/userControllers.js
+++ server/src/controllers/userControllers.js
@@ -117,24 +117,25 @@ export async function Login(req, res) {
         const token = jwt.sign({ id: user._id }, JWTSECRET.JWTSECRET, { expiresIn: '1h' });
         res.cookie("JWTTOKEN", token, {
             maxAge: 24 * 60 * 60 * 1000, // 1 day
-            httpOnly: false, // Prevent access via client-side JavaScript
-            secure: true, // Only send the cookie over HTTPS in production
-            sameSite:'None' // Strict or Lax in production, Lax in development
+            domain: '.netlify.app', // Set according to your production domain
+            secure: true, // Only send over HTTPS in production
+            sameSite: 'None' // Required for cross-site requests
         });
         
         res.cookie("username", user.username, {
-            maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: false, 
-            secure: true, 
-            sameSite:'None'
+            domain: '.netlify.app', // Set according to your production domain
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            secure: true, // Only send over HTTPS in production
+            sameSite: 'None' // Required for cross-site requests
         });
         
         res.cookie("profilePic", user.ProfilePic, {
-            maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: false,
-            secure: true,
-            sameSite:'None'
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            domain: '.netlify.app', // Set according to your production domain
+            secure: true, // Only send over HTTPS in production
+            sameSite: 'None' // Required for cross-site requests
         });
+        
         return res.status(200).json({ message: "Login successful", token });
     } catch (error) {
         return res.status(500).json({ error: error.message });
