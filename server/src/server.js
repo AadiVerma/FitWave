994443b8964eeafdc7f62ef64f8acd93e61eeafa
Aditya Verma <adityakaplish11@gmail.com>
2024-09-04 13:35:09 +0530

    more changes

--- server/src/server.js
+++ server/src/server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 // })
 app.use(sessionConfig);
 app.use(cors({
-    origin:"http://localhost:5173", 
+    origin:"fitwave-smartwave.netlify.app", 
     credentials:true
 }));
 // app.get('/', (req, res) => {
